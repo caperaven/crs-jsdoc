090e2dfc3a2dfbbfdebaed8dd90de671cb4c27a4
caperaven <caperaven@gmail.com>
2023-01-25 00:14:55 +0200

    initial

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@ mod process_file;
 
 use args::get_args;
 use std::env;
+use crate::args::Args;
 
 fn main() {
     let def = get_args();
 
     if def.file.is_some() {
         process_file(def);
+        return;
+    }
+
+    if def.folder.is_some() {
+        process_folder(def, "./");
+        return;
     }
 }
 
+/**
+* Process a file's content line by line
+* Generate a output file based on the jsdoc comments
+*/
 fn process_file(args_def: args::Args) {
     let file = args_def.file;
 
@@ -23,3 +34,11 @@ fn process_file(args_def: args::Args) {
     });
 }
 
+/**
+* Process the files in the folder recursively.
+* For each file in the folder call process_file.
+* Call this function recursively for each subfolder.
+*/
+fn process_folder(args_def: args::Args, path: &str) {
+    todo!()
+}
